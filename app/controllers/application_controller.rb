e0ae3d24857fcf7d38136c62fbfea59f9444121f
Vanshika Kumrawat <vanshika.kumrawat@protonshub.in>
2023-09-13 18:52:54 +0530

    implemented login for different types of account with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  protected
+
   def after_sign_in_path_for(_resource)
     accounts_path
   end
+
   def after_sign_out_path_for(_resource_or_scope)
     new_account_session_path
+  end
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[type name phone_number])
+    # binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts, controllers: {registrations:'accounts/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :accounts
+  resources :accounts 
 
   
 end
